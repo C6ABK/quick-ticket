715ef4137d61bb5217b9595e1e16d0d59116e928
Adam Bark <40392203+C6ABK@users.noreply.github.com>
2025-05-16 12:29:07 +0100

    create ticket form, sentry, prisma client setup

---
+++ src/actions/ticket.actions.ts
@@ -0,0 +1,26 @@
+'use server'
+import * as Sentry from '@sentry/nextjs'
+
+export async function createTicket(prevState: {success: boolean; message: string}, formData: FormData): Promise<{success: boolean; message: string}> {
+    try {
+        throw new Error('Simulated prisma error for testing')
+        const subject = formData.get('subject') as string
+        const description = formData.get('description') as string
+        const priority = formData.get('priority') as string
+
+        if (!subject || !description || !priority) {
+            Sentry.captureMessage('Validation Error: Missing ticket fields', 'warning')
+
+            return { success: false, message: "All fields are required"}
+        }
+
+        return {success: true, message: 'Ticket created successfully'}
+    } catch (error) {
+        Sentry.captureException(error as Error, {
+            extra: { formData: Object.fromEntries(formData.entries())}
+        })
+        return { success: false, message: "An error occurred while creating the ticket"}
+
+    }
+    
+}
